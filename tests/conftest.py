97aa95785ebf1f9f489016663804440f10c7e364
Alen Skorobogatov <alen0083@gmail.com>
2025-02-10 21:34:47 +0300

    Добавлены комментарии к строкам

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,11 @@ import pytest
 from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture()
+@pytest.fixture() ## Предусловие запуска браузера в автотестах: какой браузер, в каком режиме (оконный или полный и тд)
 def browser():
     options = Options()
     options.add_argument('--headless')
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
-    browser.implicitly_wait(3)
+    browser.implicitly_wait(3) ##  ожидания в случае долгой загрузки страницы
     yield browser
