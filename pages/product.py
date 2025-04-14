6293488593021bc04d618c9c56a7f1cfa155aac9
Alen Skorobogatov <alen0083@gmail.com>
2025-04-14 19:28:55 +0300

    Добавлены api тесты авторизации (получение токена) и просмотра списка ТС (ЛК ГП)

--- pages/product.py
+++ pages/product.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
-class ProductPage:
+class ProductPage: ##Сущность страницы с мониторами с описанными функциями и готовыми действиями для тестов
 
-    def __init__(self, browser):
+    def __init__(self, browser): ##Объявление функции браузера через который будут открываться стр. и осуществляться действия
         self.browser = browser
 
     def check_title_is(self, title):
-            page_title = self.browser.find_element(By.CSS_SELECTOR, 'h2')
-            assert page_title.text == title
+            page_title = self.browser.find_element(By.CSS_SELECTOR, 'h2') ##Поиск элемента на странице
+            assert page_title.text == title ##Ожидаемый результат (ждём, что у стр. есть заголовок)
